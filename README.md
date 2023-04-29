468a2b6d665b3fd78c0976d6a8d854a3b04f0d25
Tomás Ernst <tomasernst@Tomass-MacBook-Pro.local>
2023-04-29 05:01:06 -0400

    feat: create app and set basic features

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# movies
+# Movies App
+
+Type any movie title and get info about said movie.
+
+### To Run
+
+* First time:
+```terminal
+npm i 
+npm start
+```
+
+* Normal use:
+```terminal
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './index.css'
+
+function App() {
+  const [title, setTitle] = useState('');
+  const [movieData, setMovieData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.get(`http://www.omdbapi.com/?apikey=e0ca2e4d&t=${title}`)
+      .then(response => {
+        setMovieData(response.data);
+        setError(null);
+      })
+      .catch(error => {
+        setMovieData(null);
+        setError(error.message);
+      });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+      <h1 className="text-3xl font-bold mb-4">Movie Search</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-2">
+      <label htmlFor="title" className="sr-only">
+        Enter a movie title:
+      </label>
+      <div className="flex space-x-2">
+        <input
+          id="title"
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          className="border border-gray-400 rounded px-2 py-1 w-64"
+          placeholder="Enter a movie title"
+        />
+        <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-1">
+          Submit
+        </button>
+      </div>
+    </form>
+    {error && <p className="text-red-600">{error}</p>}
+    {movieData && (
+      <div className="border border-gray-400 rounded p-4 mt-4">
+        <h2 className="text-xl font-bold mb-2">{movieData.Title}</h2>
+        <p className="mb-2">Year: {movieData.Year}</p>
+        <p className="mb-2">Plot: {movieData.Plot}</p>
+      </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
